de893c75182eb9097af3eac158dd1330ef7cd069
Sanket <sanketkarki2060@gmail.com>
2025-01-17 21:23:57 +0545

    Refactor JWT verification in auth middleware to trim token and improve error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,13 +10,16 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     //Authorization: Bearer token
     //Mobile apps don't have cookies so getting accessToken from req.header but the data is in upper form.
     //But we don't want Bearer but just token so replacing Bearer to "" empty string so that we get token only
-
+    const cleanToken = token.trim();
     if (!token) {
       throw new ApiError(401, "Unauthorized request"); //??401 as unauthorized error???
     }
 
     //decrypt the token
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); //the decoded token has all data sent during encoding of token
+    const decodedToken = jwt.verify(
+      cleanToken,
+      process.env.ACCESS_TOKEN_SECRET
+    ); //the decoded token has all data sent during encoding of token
     //find user from token on database to verify
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
@@ -28,8 +31,8 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     req.user = user; //req object gets user property by this middleware
     next();
   } catch (error) {
-    // throw new ApiError(500, "Can't validate from access token at the moment");
-    throw new ApiError(500, "Invalid accessToken,relogin please");
+    throw new ApiError(500, "Can't validate from access token at the moment");
+    // throw new ApiError(500, "Invalid accessToken,relogin please");
   }
 });
 // Middleware to check if the user has admin role
