714b2138e9a69fcaf4ecb96e9e6099f317105362
Sanket <sanketkarki2060@gmail.com>
2025-04-13 18:08:20 +0545

    refactor: fix asyncHandler function signature and improve error handling

--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -1,13 +1,13 @@
-const asyncHandler = (fn) => async (err ,req, res, next) => {  //the actual code to add extra catch block to address left out thrown errors to stop from app crashing
-    try {
-      await fn(err, req, res, next);
-    } catch (error) {
-      res.status(error.statusCode || 500).json({
-        success: false,
-        message: error.message || "Internal Server Error",
-      });
-    }
-  };
-  
-  export { asyncHandler };
-  
+const asyncHandler = (fn) => async (req, res, next) => {
+  //the actual code to add extra catch block to address left out thrown errors to stop from app crashing
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
+
+export { asyncHandler };
