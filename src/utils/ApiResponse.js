395832722bda762c6e15e5ef03a8ab68c5d6d54b
Sanket <sanketkarki2060@gmail.com>
2025-04-17 13:02:38 +0545

    kept 400 messages as valid from ApiResponse

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,10 +1,10 @@
-class ApiResponse{
-    constructor(statusCode,data,message="Success"){
-        this.statusCode=statusCode
-        this.data=data;
-        this.message=message
-        this.success=statusCode < 400 //?? boolean code if(statusCode<400){true} else {false}
-    }
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 500; //?? boolean code if(statusCode<400){true} else {false}
+  }
 }
 
-export { ApiResponse }
+export { ApiResponse };
